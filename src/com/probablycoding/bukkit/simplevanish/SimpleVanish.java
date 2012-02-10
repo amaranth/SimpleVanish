f1ef852371ba46c55f44865d1b4b93a9ae9abeb9
Travis Watkins <amaranth@ubuntu.com>
2012-02-09 19:50:37 -0600

    clean up, persist vanish state

--- src/com/probablycoding/bukkit/simplevanish/SimpleVanish.java
+++ src/com/probablycoding/bukkit/simplevanish/SimpleVanish.java
@@ -1,69 +1,83 @@
 package com.probablycoding.bukkit.simplevanish;
 
-import java.util.HashSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SimpleVanish extends JavaPlugin {
-    public static final Logger log = Logger.getLogger("Minecraft");
-    public HashSet<Player> invisible = new HashSet<Player>();
-    private SimpleVanishListener listener = new SimpleVanishListener(this);
+public class SimpleVanish extends JavaPlugin implements Listener {
+    public List<String> invisible;
+
+    public void enableVanish(Player player, boolean force) {
+        if (invisible.add(player.getName()) || force) {
+            for (Player other : getServer().getOnlinePlayers()) {
+                if (other.hasPermission("simplevanish.showvanished")) {
+                    continue;
+                }
+                other.hidePlayer(player);
+            }
+
+            player.sendMessage(ChatColor.RED + "Poof!");
+        } else {
+            player.sendMessage(ChatColor.RED + "You are already vanished!");
+        }
+    }
+
+    public void disableVanish(Player player, boolean force) {
+        if (invisible.remove(player.getName()) || force) {
+            for (Player other : getServer().getOnlinePlayers()) {
+                other.showPlayer(player);
+            }
+
+            player.sendMessage(ChatColor.RED + "You have reappeared!");
+        } else {
+            player.sendMessage(ChatColor.RED + "You are not vanished!");
+        }
+    }
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(listener, this);
-        sendToLog(Level.INFO, getDescription().getVersion() + " enabled");
+        getServer().getPluginManager().registerEvents(this, this);
+        invisible = getConfig().getStringList("vanished");
     }
 
     @Override
     public void onDisable() {
-        sendToLog(Level.INFO, getDescription().getVersion() + " disabled");
+        getConfig().set("vanished", invisible);
+        saveConfig();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String name, String[] args) {
-        boolean result = false;
+        if (!(sender instanceof Player))
+            return false;
 
-        if (sender instanceof Player) {
-            if (command.getName().equalsIgnoreCase("vanish")) {
-                enableVanish((Player) sender);
-                result = true;
+        if (command.getName().equalsIgnoreCase("vanish"))
+            enableVanish((Player) sender, false);
+        else if (command.getName().equalsIgnoreCase("unvanish"))
+            disableVanish((Player) sender, false);
+
+        return true;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (invisible.contains(event.getPlayer().getName()))
+            enableVanish(event.getPlayer(), true);
+
+        if (event.getPlayer().hasPermission("simplevanish.showvanished"))
+            return;
+
+        for (String hidden : invisible) {
+            Player hiddenPlayer = getServer().getPlayerExact(hidden);
+            if (hiddenPlayer != null) {
+                event.getPlayer().hidePlayer(hiddenPlayer);
             }
-            else if (command.getName().equalsIgnoreCase("unvanish")) {
-                disableVanish((Player) sender);
-                result = true;
-            }
         }
-
-        return result;
-    }
-
-    public void sendToLog(Level level, String message) {
-        log.log(level, "[" + getDescription().getName() + "] " + message);
-    }
-
-    public void enableVanish(Player player) {
-        for (Player other : getServer().getOnlinePlayers()) {
-            if (other.isOp())
-                continue;
-            other.hidePlayer(player);
-        }
-
-        invisible.add(player);
-        player.sendMessage(ChatColor.RED + "Poof!");
-    }
-
-    public void disableVanish(Player player) {
-        for (Player other : getServer().getOnlinePlayers()) {
-            other.showPlayer(player);
-        }
-
-        invisible.remove(player);
-        player.sendMessage(ChatColor.RED + "You have reappeared!");
     }
 }
